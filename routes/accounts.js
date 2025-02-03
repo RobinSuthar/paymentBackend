2c9c2624cc62c32082df5fcb179fa30f8468374f
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-02-02 23:51:18 -0700

    Added Session to Make Sure Transactions Proceed Nicely

--- routes/accounts.js
+++ routes/accounts.js
@@ -16,11 +16,27 @@ router.post("/transfer", authMiddleware, async function (req, res) {
   const userId = req.userId;
   const { accountTo, amount } = req.body;
 
-  const acoountFrom = await Transactions.findOne({
+  const session = Transactions.startSession();
+
+  (await session).startTransaction();
+  const AccountTo = await Transactions.findOne({
     userId: req.userId,
   });
 
-  if (acoountFrom.balance < amount) {
+  if (!AccountTo.userId) {
+    (await session).abortTransaction();
+    res.json({
+      msg: "There is no Account, Please Check Again",
+    });
+
+    return;
+  }
+
+  const AccountFrom = await Transactions.findOne({
+    userId: req.userId,
+  });
+
+  if (AccountFrom.balance < amount) {
     res.status(400).json({
       msg: "Insufficient Funds",
     });
@@ -28,9 +44,25 @@ router.post("/transfer", authMiddleware, async function (req, res) {
     return;
   }
 
-  res.json({
-    msg: result.balance,
-  });
+  try {
+    AccountTo.updateOne({
+      $inc: { balance: -amount },
+    });
+
+    AccountFrom.updateOne({
+      $inc: { balance: amount },
+    });
+    res.json({
+      msg: "Money Transfered Successfully",
+    });
+
+    (await session).commitTransaction();
+    return;
+  } catch (err) {
+    res.json({
+      msg: err,
+    });
+  }
 });
 
 export default router;
