454bdee56b8d19b1ca310798bbf7d2a1bea646e9
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-02-02 23:13:23 -0700

    Added Router for Accoutns transacatinos

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userRouter from "./user.js";
+import accountRouter from "./accounts.js";
 
 const router = express.Router();
 
 router.use("/user", userRouter);
+router.use("/accounts", accountRouter);
 
 export default router;
